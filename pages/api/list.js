7d76f116621ebb96f08b0494d78a4cfa3b838e25
Josh Mo <joshua.om.876@gmail.com>
2022-09-06 20:08:21 +0100

    first commit

---
+++ pages/api/list.js
@@ -0,0 +1,10 @@
+export default async (req, res) => {
+    const url = `https://${process.env.REDIS_URL}/lrange/todo/0/100?_token= + ${process.env.REDIS_TOKEN}`
+  
+    return fetch(url)
+      .then((r) => r.json())
+      .then((data) => {
+        let result = JSON.stringify(data.result);
+        return res.status(200).json(result);
+      });
+  };
---
+++ pages/api/remove.js
@@ -0,0 +1,17 @@
+export default async (req, res) => {
+    if (!req.query.todo) {
+      return res.status(400).send("todo parameter required.");
+    }
+    let todo = encodeURI(req.query.todo);
+  
+    const token = "85d405e4ff7a49b6ab20de9188a03d1c";
+    const url =
+      `https://${process.env.REDIS_URL}/lrem/todo/1/" + todo + "?_token=" + ${process.env.REDIS_TOKEN}`;
+  
+    return fetch(url)
+      .then((r) => r.json())
+      .then((data) => {
+        let result = JSON.stringify(data.result);
+        return res.status(200).json(result);
+      });
+  };
